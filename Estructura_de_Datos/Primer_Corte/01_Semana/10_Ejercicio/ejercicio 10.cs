2359a4d4a34e7cdf11a5a12778de0c7ac0cca518
CJhan3312 <99096988+CJhan3312@users.noreply.github.com>
2022-03-09 18:19:36 -0500

    Add files via upload

---
+++ Estructura_de_Datos/Primer_Corte/01_Semana/10_Ejercicio/ejercicio 10.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.collections.Generic;
+
+namespace AplicacionNumero1
+{
+    class Program
+{
+    static void Main(string[] args)
+{key = "iloveyou123"
+password = input("Introduce la contraseña: ")
+if key == password.lower():
+    print("La contaseña coincide")
+else:
+    print("La contraseña no coincide")
+    
+}
+}
+}
---
+++ Estructura_de_Datos/Primer_Corte/02_Semana/README.md
@@ -0,0 +1,2 @@
+# Ejercicio_Semanal_2
+Segundo ejercicio semanal de Estructura de Datos
